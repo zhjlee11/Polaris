92ff1977ec0f875e50799173dd4114a8c37a3d49
zhjlee11 <zhjlee11@naver.com>
2019-11-23 03:58:37 +0900

    v0.4.2 | 출시 전 | README.md 작성 & 디펜스 파티 베이스 코드 일부 코딩

---
+++ Net RPGXP Project - Server/NRP Server/Group/DefenseGame.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRP_Server.Group
+{
+    class DefenseGame
+    {
+
+        public static Dictionary<int, DefenseGame> games = new Dictionary<int, DefenseGame>();
+
+        public int no { get; set; }
+        public UserCharacter member { get; set; }
+
+        public int GameType { get; set; }
+        /*
+         * [게임 타입]
+         * 
+         * 0. 일반 도서관 Defense 게임
+         * 
+         * */
+
+        public Dictionary<int, Enemy> enemies = new Dictionary<int, Enemy>();
+
+        public DefenseGame(UserCharacter u, int type=0) {
+            no = games.Count();
+            member = u;
+            GameType = type;
+            games[no] = this;
+        }
+
+        public void loadenemies() {
+            if (GameType == 0) {
+
+            }
+        }
+
+
+
+    }
+}
